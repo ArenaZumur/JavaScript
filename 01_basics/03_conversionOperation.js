cbe6eccbb33376163561e41695e7cd7c4e8985e6
Arena Zumur <149574018+ArenaZumur@users.noreply.github.com>
2024-09-13 17:24:29 +0000

    dt conversion and operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,31 @@ let booleanisLoggedIn=Boolean(isLoggedIn)
 let someNumber=33
 
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//****************Operations**********************//
+
+let value=3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Arena"
+str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
